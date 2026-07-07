b947756983c6982f640488bfe104f3ead52d9360
KKTNT <KKTNT@users.noreply.github.com>
2026-07-07 18:05:01 +0800

    Add say_bye function

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@ def greet(name):
     """跟某人打个招呼"""
     return f"Hello, {name}! 👋"
 
+def say_bye(name):
+    """跟某人说再见"""
+    return f"Goodbye, {name}! 👋"
+
 if __name__ == "__main__":
     print(greet("KKTNT"))
+    print(say_bye("KKTNT"))
     print("这是我用 CodeM 推到 GitHub 的第一个文件 🎉")
